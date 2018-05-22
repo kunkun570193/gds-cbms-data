ca62e9b5395925c512ec8b10fc1c6d1e8995a6e2
wyk <wangyukun@rigorin.com>
2018-05-22 18:52:03 +0800

    增加机房配置和机房认证设计

--- agent/packages/bms_auth_py/bms_auth_py.py
+++ agent/packages/bms_auth_py/bms_auth_py.py
@@ -1,18 +1,33 @@
 # -*- coding:utf-8 -*-
-import time
+import datetime,time
+import configparser
+import requests
+
 
 class BmsAuth():
     """Bms认证处理类"""
+
     def __init__(self):
         """ 构造函数 """
 
-        self.bmsCfg = None      # bms 配置
-        self.tokenInfo = None   # token 信息
+        self.bmsCfg = None  # bms 配置
+        self.tokenInfo = None  # token 信息
 
     def prepare(self):
         """ 准备运行 """
 
         # TODO ：1. 从 config/bms.cfg 文件中读取BMS配置， 失败返回 False
+        conf = configparser.ConfigParser()
+        conf.read("bms.cfg")
+        self.bmsCfg = conf.sections()  # 得到所有的section，并以列表的形式返回
+        if self.bmsCfg is None:
+            return False
+            # 获得指定section中的key的value
+        self.url = conf.get("bms_cfg_msg", "URL_ROOT")
+        self.user = conf.get("bms_cfg_msg", "USER")
+        self.password = conf.get("bms_cfg_msg", "PASSWORD")
+        self.system = conf.get("bms_cfg_msg", "SYSTEM")
+        self.version = conf.get("bms_cfg_msg", "VERSION")
 
         return True
 
@@ -20,20 +35,40 @@ class BmsAuth():
         """ 结束运行 """
         return True
 
-    def run(self):
-        # TODO ：1. 调用登录接口，取得token 信息
+    def login(self):
+        url = self.url
+        data = self.user + self.password + self.system + self.version
+        # 发送请求, 接受响应
+        res = requests.post(url=self.url, data=data)
+        # 提取token数据
+        self.tokenInfo = res.json()['token']
+        self.token_time=datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-        # TODO ：2. Token信息保存到 temp/token.txt 文件中
+        while True:
+            with open("temp/token.txt", "w")as f:
+                f.write(self.tokenInfo)
+
+    def run(self):
+        self.login()
+
+    # TODO ：1. 调用登录接口，取得token 信息
+
+    # TODO ：2. Token信息保存到 temp/token.txt 文件中
 
     def waitNextTime(self):
-        # TODO ：根据获取Token的有效期，等待相应的时间（考虑到下次操作的时间间隔，需提前10s运行）
+        time.sleep(590)
+
+
+# TODO ：根据获取Token的有效期，等待相应的时间（考虑到下次操作的时间间隔，需提前10s运行）
+
 
 def main():
     bmsAuth = BmsAuth()
-    while True :
+
+    while True:
 
         # 初始化
-        if bmsAuth.onStart():
+        if bmsAuth.prepare():
             # 运行
             bmsAuth.run()
 
@@ -41,9 +76,10 @@ def main():
             bmsAuth.end()
 
             # 等待下次运行
-            bmsAuth.sleepToNextTime()
-        else :
+            bmsAuth.waitNextTime()
+        else:
             time.sleep(3)
+
 
 if __name__ == '__main__':
     main()
